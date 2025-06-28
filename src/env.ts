56a6a47e251996ad9c76cad0648b6a6e74775cc9
Yale Araujo <yale850@gmail.com>
2025-06-28 20:42:23 -0300

    chore: setup to handle jwt and cookies in auth process

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,8 @@ const envSchema = z.object({
   AUTH_REDIRECT_URL: z.string().url().min(1),
   POSTGRES_USER: z.string(),
   POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string()
+  POSTGRES_DB: z.string(),
+  JWT_SECRET_KEY: z.string()
 })
 
 
