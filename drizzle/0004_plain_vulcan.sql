e8436bb44a0284f9d7aa90d9321bc001e08cdfe5
Yale Araujo <yale850@gmail.com>
2025-06-28 20:25:18 -0300

    feat: schema to store auth links

---
+++ drizzle/0004_plain_vulcan.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "auth_links" (
+	"id" text PRIMARY KEY NOT NULL,
+	"code" text NOT NULL,
+	"user_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "auth_links_code_unique" UNIQUE("code")
+);
+--> statement-breakpoint
+ALTER TABLE "auth_links" ADD CONSTRAINT "auth_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,2 +1,3 @@
 export * from "./restaurants";
 export * from "./users";
+export * from './auth-links'
