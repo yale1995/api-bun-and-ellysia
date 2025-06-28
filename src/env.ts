c289e78197da9bb5591d5f271374db338fa38c59
Yale Araujo <yale850@gmail.com>
2025-06-28 17:34:23 -0300

    chore: runtime type checking

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url().min(1)
+  DATABASE_URL: z.string().url().min(1),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string()
 })
+
+
 
 export const env = envSchema.parse(process.env)
