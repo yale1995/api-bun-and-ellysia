dbe70c36abc59de6b18ef89df2bc389ff6dd375f
Yale Araujo <yale850@gmail.com>
2025-06-28 20:26:35 -0300

    fix: adding schemas to db connection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "../env";
+import * as schema from "./schema";
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, { schema });
